f10a53fb3b09362908b92ecb5c1c5cf49221d9b4
Swathi HL <63439559+swathihl2@users.noreply.github.com>
2024-10-19 09:51:10 +0000

    refactor logic

--- app.py
+++ app.py
@@ -1,23 +1,26 @@
 # app.py
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return "Welcome to SHL controller"
 
+
 @app.route('/<path:path>')
 def static_files(path):
     return f"you are visiting this site{path}"
 
+
 @app.route('/stringcalc', methods=['GET', 'POST'])
-def static_files():
+def string_calc():
     data = request.get_json()
-    result = 0 
-    try; 
-      if data:
-        result = data
+    result = 0
+    try:
+        if data:
+            result = data
     except Exception as e:
-      pass
+        pass
     return f"{data} {result}"
