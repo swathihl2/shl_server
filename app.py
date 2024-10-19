32aa2890fb1ff29f12e074ca4ef505c969971d1d
Swathi HL <63439559+swathihl2@users.noreply.github.com>
2024-10-19 09:41:47 +0000

    add stringcalc endpoint

--- app.py
+++ app.py
@@ -11,9 +11,14 @@ def index():
 def static_files(path):
     return f"you are visiting this site{path}"
 
-
 @app.route('/stringcalc')
 def static_files():
     data = request.get_json()
+    result = 0 
+    try; 
+
+        if data
     
-    return f"you are visiting this site{data}"
+    except Exception as e:
+        pass
+    return f"{data} {result}"
