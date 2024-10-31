1666beac8815ebf62a9af56ca48244128a5f7d1f
Swathi HL <63439559+swathihl2@users.noreply.github.com>
2024-10-31 21:24:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 
 from controller.item_controller import item_bp
 from controller.calculator_controller import calculator_bp
-# from controller.prometheus_controller import prom_bp
+from controller.prometheus_controller import prom_bp
 from metrics.health_scheduler import start_scheduler
 
 app = Flask(__name__)
