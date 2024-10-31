82631c6185870fd635639c4a0b8261024bfecf6c
Swathi HL <63439559+swathihl2@users.noreply.github.com>
2024-10-31 23:49:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 
 from controller.item_controller import item_bp
 from controller.calculator_controller import calculator_bp
-from controller.prometheus_controller import prom_bp
+# from controller.prometheus_controller import prom_bp
 # from metrics.health_scheduler import start_scheduler
 
 app = Flask(__name__)
@@ -13,7 +13,7 @@ CORS(app)
 
 app.register_blueprint(item_bp)
 app.register_blueprint(calculator_bp)
-app.register_blueprint(prom_bp)
+# app.register_blueprint(prom_bp)
 
 
 @app.route('/')
