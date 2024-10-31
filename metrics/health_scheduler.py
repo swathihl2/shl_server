42a3496cd59115f7e1138c87e3bb38c78585aae9
Swathi HL <63439559+swathihl2@users.noreply.github.com>
2024-10-31 23:40:31 +0530

    Update health_scheduler.py

--- metrics/health_scheduler.py
+++ metrics/health_scheduler.py
@@ -1,24 +1,24 @@
-import threading
-import time
-import requests
+# import threading
+# import time
+# import requests
 
-health_status = {"status": "unknown"}
-
-
-# def start_scheduler():
-#     check_health()
+# health_status = {"status": "unknown"}
 
 
-def check_health():
-    global health_status
-    try:
-        response = requests.get('https://shl-server.onrender.com/health')
-        if response.status_code == 200:
-            health_status = {"status": "healthy", "data": response.json()}
-        else:
-            health_status = {"status": "unhealthy", "code": response.status_code}
-    except Exception as e:
-        health_status = {"status": "error", "message": str(e)}
-    time.sleep(30)
-    # Schedule the next health check
-    # threading.Timer(30, check_health).start()
+# # def start_scheduler():
+# #     check_health()
+
+
+# def check_health():
+#     global health_status
+#     try:
+#         response = requests.get('https://shl-server.onrender.com/health')
+#         if response.status_code == 200:
+#             health_status = {"status": "healthy", "data": response.json()}
+#         else:
+#             health_status = {"status": "unhealthy", "code": response.status_code}
+#     except Exception as e:
+#         health_status = {"status": "error", "message": str(e)}
+#     time.sleep(30)
+#     # Schedule the next health check
+#     # threading.Timer(30, check_health).start()
